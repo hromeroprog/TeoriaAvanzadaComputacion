0ae3698e15d3c3c92307195b957da02cd60ef6bb
Hugo Romero <100386322@alumnos.uc3m.es>
2021-02-19 16:32:39 +0100

    arreglados bugs y tests ready to launch

--- src/entrega0/Main.java
+++ src/entrega0/Main.java
@@ -1,13 +1,8 @@
 package entrega0;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -63,7 +58,7 @@ public class Main {
 		return new TestStructure(end_timer - start_timer, result);
 	}
 
-	public static void generateTest(int cases_per_size, int lowest_size, int highest_size, String test_type,
+	public static void generateTest(int cases_per_size, int lowest_size, int highest_size, String test_type,boolean only_primes,
 			String filename) throws IOException {
 		ArrayList<BigInteger> numbers = new ArrayList<>();
 		ArrayList<Long> times = new ArrayList<>();
@@ -71,22 +66,28 @@ public class Main {
 
 		for (int size_of_number = lowest_size; size_of_number < highest_size; size_of_number++) {
 			for (int i = 0; i < cases_per_size; i++) {
-				BigInteger test = Tools.getRandomBigInteger(size_of_number);
+				BigInteger test = only_primes?Tools.getRandomBigPrime(size_of_number):Tools.getRandomBigInteger(size_of_number);
 				TestStructure result = usePrimalityTest(test, test_type);
 				numbers.add(test);
 				times.add(result.time);
 				primes.add(result.isPrime);
 
-				if (i % 10 == 0)
-					System.out.println("Finished test of size: " + size_of_number + " -- Number: " + i);
+				if (i % 5 == 0)
+					System.out.println("Finished test of size: " + size_of_number + " -- Number: " + i + " -- Elapsed time: " + result.time);
 			}
 			System.out.println("Finished tests of size: " + size_of_number);
 		}
-		Tools.exportCSV("./reduction_files/" + filename + ".csv", numbers, times, primes);
+		Tools.exportCSV("./primality_files/" + filename + ".csv", numbers, times, primes);
 		System.out.println("File exported successfully");
 	}
 
 	public static void main(String[] args) throws IOException {
-		generateTest(40, 3, 12, "basic", "basic");
+		
+		generateTest(50, 3, 10, "basic", false,"basic_comp");
+		generateTest(50, 3, 10, "basic", true,"basic_primes");
+		generateTest(50, 3, 18, "improved_1", false,"improved_1_comp");
+		generateTest(50, 3, 18, "improved_1", true,"improved_1_primes");
+		generateTest(50, 3, 18, "AKS", false, "AKS_comp");
+		generateTest(50, 3, 18, "AKS", true, "AKS_primes");
 	}
 }
