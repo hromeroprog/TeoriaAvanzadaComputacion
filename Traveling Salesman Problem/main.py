c97efefe36516441cd0720802d3f4592969832bb
Hugo Romero <100386322@alumnos.uc3m.es>
2021-05-05 02:16:55 +0200

    limpiado, comentado y dandole gracia las graficas

--- Traveling Salesman Problem/main.py
+++ Traveling Salesman Problem/main.py
@@ -4,15 +4,14 @@ Created on Tue May  4 12:02:52 2021
 
 @author: 2rome
 """
-from random import shuffle
+
 from TSP import TSP
 
 if __name__ == '__main__':
-    tsp = TSP()
+    tsp = TSP() #Crear el objeto
     fichero = "ulysses16.tsp"
-    tsp.obtener_desde_archivo_tsp(fichero)
-    tsp.solve()
-    tsp.draw_with_solution()
-    shuffle(tsp.solution)
-    tsp.draw_with_solution()
-
+    #tsp.obtener_desde_archivo_tsp(fichero)  #Lee de un archivo .tsp
+    tsp.obtener_random(30) #Genera un escenario aleatorio de la dimension que le pases
+    tsp.solve() #Genera una solucion de ruta
+    tsp.draw_with_solution() #dibujar la solucion
+    tsp.draw() #dibujar solo las ciudades
--- Traveling Salesman Problem/tsp.py
+++ Traveling Salesman Problem/tsp.py
@@ -5,27 +5,41 @@ Created on Tue May  4 12:43:59 2021
 @author: 2rome
 """
 from matplotlib import pyplot as plt
+import random
 
 class TSP:
     def __init__(self):
+        self.nombre = ''
         self.dimension = 0
         self.problema = {}
         self.solution = []
         self.figures = 0
         
-        
 
     def obtener_desde_archivo_tsp(self, tsp_name):
         tsp_file = f"./TSP_interesantes/{tsp_name}"
         lines = self.read_file(tsp_file)
+        self.nombre = [line.partition('NAME:')[2] for line in lines if 'NAME: ' in line][0].strip()
         self.dimension = int([line.partition('DIMENSION:')[2] for line in lines if 'DIMENSION: ' in line][0])
         index_for_search = [index for index,line in enumerate(lines) if 'NODE_COORD_SECTION' in line][0]  +1
-        cities_data = lines[index_for_search:index_for_search+self.dimension]
+        cities_data = lines[index_for_search:index_for_search + self.dimension]
+        self.problema = {}
         for city in cities_data:
             idx,x,y = map(float, city.split(' '))
             self.problema[int(idx)] = (x,y)
         
         print(f'Fichero {tsp_name} parseado con exito')
+        
+    def obtener_random(self, dimension):
+        self.nombre = f'Aleatorio {dimension} dimensiones'
+        self.dimension = dimension
+        self.problema = {}
+        for i in range(1, dimension+1):
+            self.problema[i] =  round(random.random()*50,2), round(random.random()*50, 2)
+        
+            
+        
+        
         
     def read_file(self, file):
         with open(file, 'r') as reader:
@@ -41,17 +55,25 @@ class TSP:
     
     def solve(self):
         self.solution = list(self.problema.keys())
+        random.shuffle(self.solution)
     
     def draw(self):
         x = [coord[0] for coord in self.problema.values()]
         y = [coord[1] for coord in self.problema.values()]
         names = list(self.problema.keys())
-        plt.figure(self.figures)
+        
+        width = 9.6
+        height = 7.2
+        bool_dim = self.dimension > 20
+        figsize = [width+(width*bool_dim), height+(height*bool_dim)]
+        plt.figure(self.figures, figsize = figsize)
+        
         self.figures+=1
         plt.scatter(x,y,s=15, marker = 'x', c = 'black')
         for txt,x_coord, y_coord in zip(names, x, y):
             plt.annotate(txt, (x_coord, y_coord))
         plt.xlim(min(x)-1, max(x) +1)
+        plt.suptitle(f'{self.nombre} sin solucion', fontsize = 14)
     
     def draw_with_solution(self):
         self.draw()
@@ -62,6 +84,8 @@ class TSP:
             print(x_values)
             print(y_values)
             plt.plot(x_values, y_values, 'red')
+        plt.suptitle(f'{self.nombre} con solucion', fontsize = 14)
+        plt.title(', '.join(map(str, self.solution)), fontsize = 10)
             
         
         
